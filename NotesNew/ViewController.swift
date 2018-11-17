625c0b8fd3f90d96853ccbf9794ea813919e6325
Smitha Kolan <smitkolan@gmail.com>
2018-11-17 21:01:35 +0100

    Initial Commit

---
+++ NotesNew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesNew
+//
+//  Created by Smitha Kolan on 11/17/18.
+//  Copyright © 2018 Smitha Kolan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
